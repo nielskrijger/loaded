49d62f2522dce668cdbeabc00527c8b8cf1985d1
Niels Krijger <niels.krijger@icemobile.com>
2014-03-31 09:12:51 +0200

    * Added unit test for single beforeAll, afterAll and test function.

--- test/lib/Suite.js
+++ test/lib/Suite.js
@@ -4,51 +4,6 @@ var Suite = require('../../lib/Suite');
 var stopwatch = require('../../lib/timer');
 var assert = require('chai').assert;
 
-var test = new Suite('test', 50, 5);
-var beforeAllCount = 0;
-var testCount = 0;
-var afterAllCount = 0;
-
-test.beforeAll([
-    function (next) {
-        next(null, 'test');
-    },
-    function (var1, next) {
-        assert.equal(var1, 'test');
-        beforeAllCount++;
-        next();
-    }
-]);
-test.test([
-    function (n, next) {
-        var timer = stopwatch.start('test.1');
-        setTimeout(function () {
-            timer.stop();
-            next(null, 'test');
-        }, Math.round(Math.random() * 10));
-    },
-    function (var1, next) {
-        testCount++;
-        assert.equal(var1, 'test');
-        var timer = stopwatch.start('test.2');
-        setTimeout(function () {
-            timer.stop();
-            next();
-        }, Math.round(Math.random() * 10));
-    }
-]);
-
-test.afterAll([
-    function (next) {
-        next(null, 'test');
-    },
-    function (var1, next) {
-        assert.equal(var1, 'test');
-        afterAllCount++;
-        next();
-    }
-]);
-
 describe('lib/Suite.js', function () {
 
     beforeEach(function () {
@@ -56,8 +11,77 @@ describe('lib/Suite.js', function () {
     });
 
     describe('#run()', function () {
-        it('should run all beforeAll functions, afterAll functions and tests', function (done) {
+        it('should run multiple beforeAll functions, afterAll functions and tests', function (done) {
+            var test = new Suite('test', 50, 5);
+            var beforeAllCount = 0;
+            var testCount = 0;
+            var afterAllCount = 0;
+
+            test.beforeAll([
+                function (next) {
+                    next(null, 'test');
+                },
+                function (var1, next) {
+                    assert.equal(var1, 'test');
+                    beforeAllCount++;
+                    next();
+                }
+            ]);
+            test.test([
+                function (n, next) {
+                    var timer = stopwatch.start('test.1');
+                    setTimeout(function () {
+                        timer.stop();
+                        next(null, 'test');
+                    }, Math.round(Math.random() * 10));
+                },
+                function (var1, next) {
+                    testCount++;
+                    assert.equal(var1, 'test');
+                    var timer = stopwatch.start('test.2');
+                    setTimeout(function () {
+                        timer.stop();
+                        next();
+                    }, Math.round(Math.random() * 10));
+                }
+            ]);
+            test.afterAll([
+                function (next) {
+                    next(null, 'test');
+                },
+                function (var1, next) {
+                    assert.equal(var1, 'test');
+                    afterAllCount++;
+                    next();
+                }
+            ]);
             test.run(function (err) {
+                assert.equal(beforeAllCount, 1);
+                assert.equal(testCount, 50);
+                assert.equal(afterAllCount, 1);
+                done();
+            });
+        });
+
+        it('should run a single beforeAll function, afterAll function and test', function (done) {
+            var test2 = new Suite('test', 50, 5);
+            var beforeAllCount = 0;
+            var testCount = 0;
+            var afterAllCount = 0;
+            test2.beforeAll(function (next) {
+                beforeAllCount++;
+                next(null, 'test');
+            });
+            test2.test(function (n, next) {
+                testCount++;
+                next(null, 'test');
+            });
+
+            test2.afterAll(function (next) {
+                afterAllCount++;
+                next(null, 'test');
+            });
+            test2.run(function (err) {
                 assert.equal(beforeAllCount, 1);
                 assert.equal(testCount, 50);
                 assert.equal(afterAllCount, 1);
--- test/lib/stats.js
+++ test/lib/stats.js
@@ -28,8 +28,8 @@ describe('lib/stats.js', function () {
                     assert(report['a'].sum >= 2210 && report['a'].sum < 2400);
                     assert(report['a'].median >= 20 && report['a'].median <= 25);
                     assert(report['a'].mean >= 20 && report['a'].mean <= 25);
-                    assert(report['a'].min >= 20 && report['a'].min <= 21);
-                    assert(report['a'].max >= 50 && report['a'].min <= 51);
+                    assert(report['a'].min >= 20 && report['a'].min <= 25);
+                    assert(report['a'].max >= 50 && report['a'].min <= 55);
                     assert(report['a']['90percentile'] >= 20 && report['a']['90percentile'] < 50);
                     assert(report['a']['95percentile'] >= 50);
                     assert(report['a']['99percentile'] >= 50);
@@ -37,8 +37,8 @@ describe('lib/stats.js', function () {
                     assert(report['a.b'].sum >= 1960 && report['a.b'].sum < 2200);
                     assert(report['a.b'].median >= 20 && report['a.b'].median <= 25);
                     assert(report['a.b'].mean >= 20 && report['a.b'].mean <= 25);
-                    assert(report['a.b'].min >= 20 && report['a.b'].min <= 21);
-                    assert(report['a.b'].max >= 20 && report['a.b'].min <= 21);
+                    assert(report['a.b'].min >= 20 && report['a.b'].min <= 25);
+                    assert(report['a.b'].max >= 20 && report['a.b'].min <= 25);
                     assert(report['a.b']['90percentile'] >= 20);
                     assert(report['a.b']['95percentile'] >= 20);
                     assert.equal(report['a.b']['99percentile'], null);
